d096dc32b0f7fe8aea6f0ad5277a1f2f17ad1c07
kimbroughd <31328710+kimbroughd@users.noreply.github.com>
2017-09-19 00:30:15 -0400

    Update Document.java

--- Document.java
+++ Document.java
@@ -1,7 +1,13 @@
 public abstract class Document {
         String name;
+        String type;
         
-        void createDocument() {
-              System.out.println("Creating " + name);
-              }
+        public Document(String documentName) {
+                name = documentName;
+        }
+        public void setType(String type) {
+              this.type = type;
+        }
+        public void change() {
+                System.out.println("Document " + name + " changed.")
  }
